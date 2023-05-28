305e223e9cd56b6f89d219f807f6bfcdb17d92ae
pepezamarripa <pepe.zamarripa@gmail.com>
2023-05-28 12:06:03 -0600

    Commit code updates

---
+++ Plot_1.R
@@ -0,0 +1,26 @@
+# Pepe Zamarrips
+# Exploratory Data Analysis - Project 2
+# May 28th, 2023
+
+# Plot 1
+library("data.table")
+
+path <- getwd()
+# Download the zip with the DBs
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+# Create the tables
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+
+# Change "Emissions" to numeric
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+#Get the total NEI
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+#Plot the total NEI
+barplot(totalNEI[, Emissions], names = totalNEI[, year], xlab = "Years", ylab = "Emissions", main = "Emissions over the Years")
